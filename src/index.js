464aec3a5680af2db00251b7c431c9b65d6030cb
Marina Zalozna <marina.zalozna@gmail.com>
2022-11-19 18:34:28 +0100

    HW

---
+++ src/index.js
@@ -0,0 +1,72 @@
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries';
+
+const DEBOUNCE_DELAY = 300;
+
+const inputRef = document.getElementById('search-box');
+const listRef = document.querySelector('.country-list');
+const infoRef = document.querySelector('.country-info');
+
+const cleanMarkup = ref => (ref.innerHTML = '');
+
+const inputHandler = e => {
+  const textInput = e.target.value.trim();
+
+  if (!textInput) {
+    cleanMarkup(listRef);
+    cleanMarkup(infoRef);
+    return;
+  }
+
+  fetchCountries(textInput)
+    .then(data => {
+      console.log(data);
+      if (data.length > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name');
+        return;
+      }
+      renderMarkup(data);
+    })
+    .catch(err => {
+      cleanMarkup(listRef);
+      cleanMarkup(infoRef);
+      Notify.failure('Oops, there is no country with that name');
+    });
+};
+
+const renderMarkup = data => {
+  if (data.length === 1) {
+    cleanMarkup(listRef);
+    const markupInfo = createInfoMarkup(data);
+    infoRef.innerHTML = markupInfo;
+  } else {
+    cleanMarkup(infoRef);
+    const markupList = createListMarkup(data);
+    listRef.innerHTML = markupList;
+  }
+};
+
+const createListMarkup = data => {
+  return data
+    .map(
+      ({ name, flags }) =>
+        `<li><img src="${flags.png}" alt="${name.official}" width="60" height="40">${name.official}</li>`,
+    )
+    .join('');
+};
+
+const createInfoMarkup = data => {
+  return data.map(
+    ({ name, capital, population, flags, languages }) =>
+      `<h1><img src="${flags.png}" alt="${name.official}" width="40" height="40">${
+        name.official
+      }</h1>
+      <p>Capital: ${capital}</p>
+      <p>Population: ${population}</p>
+      <p>Languages: ${Object.values(languages)}</p>`,
+  );
+};
+
+inputRef.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
